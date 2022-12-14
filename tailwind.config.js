4ea610b7c4ad4b5c166ec1fdd6cf187b158f867b
Yu-cheng Zheng <yu-chengzheng@Yu-chengs-MacBook-Air.local>
2022-12-14 11:16:24 +0800

    First Git

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      color: {
+        primart: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      },
+      maxWidth: {
+        '1/4': '25%',
+        '1/2': '10rem',
+        '3/4': '75%',
+      },
+      spacing: {
+        '128': '32rem',
+        '104': '26rem'
+      },
+      width: {
+        '10': '2.5rem',
+      }
+    },
+  },
+  plugins: [],
+}
